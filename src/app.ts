0acbd08f5f8df9c35fe73b6646958fb7dce318dc
adnan-akhlas <mail.adnanakhlas@gmail.com>
2026-03-11 15:16:32 +0600

    feat(auth): initialized better-auth with credential authentication

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import globalRouter from "./router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/api/v1", globalRouter);
 
 app.get("/", (req: Request, res: Response) => {
